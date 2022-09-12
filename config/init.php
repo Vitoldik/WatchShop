8837b227f8175ae037087a597b3cef96fd8c5479
Vitaliy <fruitamin@yandex.ru>
2022-09-12 21:41:53 +0300

    Созданы маршруты и конфиг

---
+++ config/init.php
@@ -0,0 +1,26 @@
+<?php
+
+const DEBUG = true;
+const ROOT_DIR = __DIR__ . '/..';
+const PUBLIC_DIR = __DIR__ . '/../public';
+const APP_DIR = __DIR__ . '/../app';
+const CORE_DIR = __DIR__ . '/../vendor/watchShop/core';
+const LIBS_DIR = __DIR__ . '/../vendor/watchShop/core/libs';
+const CACHE_DIR = __DIR__ . '/../tmp/cache';
+const CONFIG_DIR = __DIR__ . '/../config';
+
+const TEMPLATE = 'default';
+
+const PROTOCOL = 'http://';
+// http://localhost/public/index.php
+$appPath = PROTOCOL . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+// http://localhost/public/
+$appPath = preg_replace("#[^/]+$#", '', $appPath);
+// http://localhost
+$appPath = str_replace('/public/', '', $appPath);
+
+define("MAIN_URL", $appPath);
+
+const ADMIN_PAGE = MAIN_URL . '/admin';
+
+require_once __DIR__ . '/../vendor/autoload.php';
