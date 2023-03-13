aa9ea9ca43078b237d002297cb66405eda0a1edd
Vitaliy <fruitamin@yandex.ru>
2023-03-13 20:40:55 +0300

    Реализовано получение брендов из базы

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -8,6 +8,9 @@ use watchShop\Cache;
 class MainController extends AppController {
 
     public function indexAction() {
+        $brands = \R::find('brand', 'LIMIT 3');
+
         $this->setMeta(App::$app->getProperty('shop_name'), 'Описание', 'Ключи');
+        $this->setData(compact('brands'));
     }
 }
