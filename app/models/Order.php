c036858d70fcfa21e01ef1f1792984885e3092d0
Vitaliy <fruitamin@yandex.ru>
2023-05-07 14:35:48 +0300

    Разработка оформления заказа

--- app/models/Order.php
+++ app/models/Order.php
@@ -2,17 +2,26 @@
 
 namespace app\models;
 
+use R;
+
 class Order extends AppModel {
 
     public static function saveOrder($data) {
+        $order = R::dispense('order');
+        $order->user_id = $data['user_id'];
+        $order->note = $data['note'];
+        $order->currency = $_SESSION['cart.currency']['code'];
+        $orderId = R::store($order);
+
+        self::saveOrderProduct($orderId);
+        return $orderId;
+    }
+
+    public static function saveOrderProduct($orderId) {
 
     }
 
-    public static function saveOrderProduct($order_id) {
-
-    }
-
-    public static function mailOrder($order_id, $user_email) {
+    public static function mailOrder($orderId, $userEmail) {
 
     }
 }
